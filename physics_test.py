ae6eaf1a1346e230dce3cc5b23cdcf7998439a05
sam-031 <email>
2022-03-20 21:04:37 +0000

    PM v1.3 - added: Ozgarmas elektr toki

--- physics_test.py
+++ physics_test.py
@@ -1,9 +1,34 @@
 import random
 
-#.....
+
+# ---| [8] O'zgarmas elektr toki \---
+
+
+tok_kuchi = ["Elektrodinamika nima?","O'zgarmas tok nima?","Tok kuchi nima?","Tok zichligi nima?","Elektr toki ta'sirlari qanday?","Elektr tokini mavjud bo'lish shartlari","Tok kuchi qanday kattalik?","Tok zichligi qanday kattalik?","Zaryad konsentratsiyasi berilgan holda tok zichligini topish formulasi qanday?","Metall o'tkazgich ko'ndalang kesim yuzidan o'tgan elektronlar sonini topish formulasi qanday?","Metallarda zaryadning tartibli harakat tezligini topish formulasi qanday?"]
+
+zbq_om_qonuni = ["Tok kuchining kuchlanishga bog'liqlik grafigidan qarshilik qanday topiladi?","Zanjirning bir qismi uchun Om qonuni qanday?","Elektr qarshilik birligini XBS dagi asosiy birliklar orqali ifodalang."]
+
+qarshilik = ["Bir Om ta'rifi - ?","O'ta-o'tkazuvchanlik hodisasi nima?","Qarshilik termometrlari nima?","Elektr qarshilik nima sababdan vujudga keladi?","Elektr qarshilik qaysi kattalikka bog'liq?","Solishtirma qarshilik nima?","Reostat nima?","Rezistor nima?","Solishtirma qarshilik birligini XBS dagi asosiy birliklar orqali ifodalang."]
+
+ketma_ket_va_parallel_ulash = ["O'tkazgichlarni ketma-ket ulash sxemasi qanday?","Otkazgichlar ketma-ket ulansa, umumiy tok kuchi/kuchlanish/qarshilik/zaryad nimaga teng?","O'tkazgichlarni parallel ulash sxemasi qanday?","O'tkazgichlarni parallel ulaganda umumiy tok kuchi/qarshilik/kuchlanish/zaryad nimaga teng?","Simni cho'zib uzaytirsak qarshiligi qanday o'zgaradi?"]
+
+shunt_ulash = ["Ampermetr zanjirga qanday ulanadi?","Voltmetr nima va u zanjirga qanday ulanadi?","Ommetr nima va u zanjirga qanday ulanadi?","Vattmetr nima va u zanjirga qanday ulanadi?","Ampermetrga shunt nima maqsadda ulanadi?","Voltmetrga shunt nima maqsadda ulanadi?","Shunt nima?"]
+
+zbq_joul_lens = ["Joul-Lens qonunini tushuntiring!","O'zgarmas tokning ishi (zanjirning 1 qismi uchun) qanday topiladi?","O'zgarmas tokning quvvati (zanjirning bir qismi uchun) qanday topiladi?"]
+
+ketma_ket_va_parallel_ish_quvvat = ["O'tkazgichlar ketma-ket/parallel ulanganda qaysi birida ko'p issiqlik ajraladi?","Spirallar ketma-ket/parallel ulanganda suvni qaynash vaqtini topish formulasi qanday?","n ta bir xil cho'lg'am ketma-ket ulanganda suvning qaynash vaqti, ular parallel ulangandagidan qancha farq qiladi?","Ketma-ket/Parallel ulangan iste'molchilarning umumiy quvvat qanday topiladi?","Quvvatlari P1 va P2 bo'lgan lampalar manbaga parallel/ketma-ket ulanganda ularning alohida quvvatlari qanday topiladi?"]
+
+toliq_om_qonuni = ["EYuK nima?","Berk zanjir uchun Om qonuni qanday?","Qisqa tutashuv nima?"]
+
+tok_manbalarini_kk_pll_ulash = ["Manbalarni ketma-ket/parallel ulash sxemasi qanday?","Elementlar ketma-ket/parallel ulanganda berk zanjir uchun Om qonuni qanday?","Akkumulyatorni zaryadlashdagi/razryadlashdagi kuchlanish","Tok manbalarining FIK qanday topiladi?","Kirxgofning I qonuni - ?","Kirxgofning II qonuni","Tarmoqlanish tugunida uchrashuvchi toklarning algebraik yig'indisi qanday topiladi?"]
+
+toliq_joul_lens = ["O'zgarmas tokning quvvati (butun zanjir uchun) qanday topiladi?","Tok manbaining foydali/samarasiz quvvati qanday topiladi?","Tok manbaini FIK sini topish formulasini isbotlang.","Tok manbaining FIK qarshiliklar/kuchlanish orqali qanday topiladi?"]
+
+ozgarmas_elektr_toki = tok_kuchi + zbq_om_qonuni + qarshilik + ketma_ket_va_parallel_ulash + shunt_ulash + zbq_joul_lens + ketma_ket_va_parallel_ish_quvvat + toliq_om_qonuni + tok_manbalarini_kk_pll_ulash + toliq_joul_lens
 
 
 # ---| [9] Turli muhitlarda elektr toki \---
+
 
 metallar = ["Metallarda asosiy tok tashuvchilar nima?","Metallarda qarshilikning temperaturaga bog'liqligi?","O'ta-o'tkazuvchanlik?","Volt-Amper xarakteristikasi nima?","Qarshilikning termik koeffitsiyenti nima?","Metallarda elektr tokining tarqalish tezligi nima bilan aniqlanadi?","Metallarda Volt-Amper xarakteristikasi qanday?"]
 
@@ -19,6 +44,7 @@ turli_muhitlarda_elektr_toki = metallar + elektrolitlar + vakuum + yarimotkazgic
 
 
 # ---| [10] Elekromagnit hodisalar \---
+
 
 magnit_maydon = ["Doimiy magnit nima?","Magnit qutblari nima?","Yerning magnit maydoni qanday yo'nalgan?","Kompas nima?","Har xil magnit qutblari bir-biriga qanday ta'sir ko'rsatadi?","Qanday magnit qutblari bir-biridan qochadi?","Elektr toki atrofida qanday maydon mavjud?","Magnit maydonni aniqlagan odam kim?","Qachon parallel toklar bir-biridan qochadi va qachon bir-biriga tortiladi?","Amper tajribasi qanday edi?","Sinov konturi nima?","Parma qoidasi nima?","Magnit momenti nima?","Magnit maydon induksiyasi nima?","Tokli ramkaga ta'sir etuvchi kuch momenti qanday topiladi?","M = BISsin@ da @ qanday burchak?","Induksiya chiziqlari nima?","Induksiya chiziqlari yopiqmi yoki ochiqmi?","Magnit maydon kuchlanganligi nima?","Magnit maydon kuchlanganligi birligi va formulasi qanday?","Magnit maydon kuchlanganligi va induksiya orasida qanday bog'liqlik mavjud?","Tabiatda magnit zaryadlari mavjudmi?","Konturning magnit momenti nimani ko'rsatadi?","Konturning magnit momentining yo'nalishi qanday aniqlanadi?","Harakatsiz zaryad atrofida magnit maydon hosil bo'ladimi?","Magnit induksiya chiziqlari magnit ichida qanday yo'nalishga ega?","Magnit induksiya chiziqlari magnit tashqarisida qanday yo'nalishga ega?","Quyosh shamoli nima?","Qutb yog'dusi qanday vujudga keladi?","Magnitosfera deb nimaga aytiladi?","Parallel toklarning o'zaro ta'siri kuchi formulasi nima?","Parallel toklarning o'zaro ta'sir kuchi nimalarga bog'liq?","Magnit doimiysi nima?","Nisbiy magnit singdiruvchanlik nima?","1 Amper ni ta'riflang.","Ersted tajribada nimani aniqlagan?","Magnit doimiysi birligini XBS dagi asosiy birliklar orqali ifodalang.","Magnit maydon induksiya birligini XBS dagi asosiy birliklar orqali ifodalang.","Magnit maydon kuchlanganligi birligini XBS dagi asosiy birliklar orqali ifodalang","Magnit maydonning elektr maydoni bilan bog'liqligini tajribada birinchi bo'lib kim aniqlagan?","Gorizontal o'q atrofida erkin aylana oladigan magnit strelka deyarli tik turib qoldi, bunda uning shimoliy uchi tepada joylashdi. Strelka Yerning qaysi joyida joylashgan?","Kompasning magnit strelkasining o'qi magnit meridianiga tik ravishda gorizontal o'rnatildi. Bunda strelka aniq gorizontal vaziyatni oldi. Kompas qayerda joylashgan?"]
 
@@ -42,7 +68,12 @@ magnit_maydon_energiyasi = []
 
 elektromagnit_hodisalar = magnit_maydon + magnit_maydonda_tokli_otkazgichga_tasir_qiluvchi_kuch + bio_salvar_laplas_qonuni + lorens_kuchi + magnit_oqimi + muhit_magnit_singdiruvchanligi + elektromagnit_induksiya + lens_qoidasi_ozinduksiya + induktivlik + magnit_maydon_energiyasi
 
+
 #.....
+
+
+
+
 
 #***********************************************
 #***********************************************
